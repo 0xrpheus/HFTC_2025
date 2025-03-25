41582f724e03fe90439fb1148cb7372c58565671
codebhav <vaibhava0801@gmail.com>
2025-03-25 00:12:30 -0400

    Updated credentials

--- run.py
+++ run.py
@@ -156,8 +156,8 @@ def main(trader):
 
 
 if __name__ == '__main__':
-    with shift.Trader("test001") as trader:
-        trader.connect("initiator.cfg", "password")
+    with shift.Trader("dolla-dolla-bills-yall") as trader:
+        trader.connect("initiator.cfg", "Zxz7Qxa9")
         sleep(1)
         trader.sub_all_order_book()
         sleep(1)
